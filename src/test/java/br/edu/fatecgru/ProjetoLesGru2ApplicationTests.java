5f7e988a3e1030518b4c2c58fb4dcee0a81ba337
Vinicius Huang <viniciushuangbr@gmail.com>
2025-05-15 10:55:34 -0300

    first commit

---
+++ src/test/java/br/edu/fatecgru/ProjetoLesGru2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecgru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLesGru2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
