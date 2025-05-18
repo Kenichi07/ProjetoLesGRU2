3c1a52f9169dac141bbf59ab4c251a3d72eb40c8
Vinicius Huang <viniciushuangbr@gmail.com>
2025-05-18 17:44:38 -0300

    Classes criadas

--- src/main/java/br/edu/fatecgru/model/entity/Usuario.java
+++ src/main/java/br/edu/fatecgru/model/entity/Usuario.java
@@ -1,5 +1,36 @@
 package br.edu.fatecgru.model.entity;
 
-public class Usuario {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Usuario {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	private String email;
+	private String senha;
+	private String tipoUsuario;
+	private String whatsapp;
+	
+	//CONTRUTORES
+	public Usuario() {
+		
+	}
+
+	public Usuario(int id, String nome, String email, String senha, String tipoUsuario, String whatsapp) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.tipoUsuario = tipoUsuario;
+		this.whatsapp = whatsapp;
+	}
+	
+	
+	
 }
