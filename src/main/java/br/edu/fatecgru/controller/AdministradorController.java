4a454deea7da43e734d19f6119359da4f85741de
Vinicius Huang <viniciushuangbr@gmail.com>
2025-05-29 11:57:55 -0300

    tela login e cadastrar

--- src/main/java/br/edu/fatecgru/controller/AdministradorController.java
+++ src/main/java/br/edu/fatecgru/controller/AdministradorController.java
@@ -1,17 +1,29 @@
 package br.edu.fatecgru.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.fatecgru.service.AdministradorService;
 
-@RestController
-//@Controller
-@RequestMapping
+//@RestController
+@Controller
+@RequestMapping("/administrador")
 public class AdministradorController {
 
 	@Autowired
 	private AdministradorService administradorService;
+
+	@GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+	
+	@GetMapping("/cadastro")
+    public String cadastroPage() {
+        return "cadastro";
+    }
 	
 }
